b0ab3a0ae99c9eb319e264d152ce5f9793bde215
Krisnu-6 <144804402+Krisnu-6@users.noreply.github.com>
2023-09-28 16:22:47 +0530

    Add files via upload

---
+++ while loop.c4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+	int n;
+	printf("enter n value=");
+	while(n>=1)
+	{
+		("%d\n",n);
+		n--;
+	}
+}
---
+++ while loop.c5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+	if(i%2==1)
+	while(n>=1)
+	{
+		printf("%d\n",i);
+		i++
+	}
+}
